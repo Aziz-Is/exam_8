c9f8325a7092e57862092714963cb51b195a22d6
Aziz_Is <Aziz10877@gmail.com>
2022-08-27 15:07:14 +0600

    add html added

--- myproject/mainapp/urls.py
+++ myproject/mainapp/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path
-from .views import ProductList
+from .views import ProductList, ProductView, AddProductView, UpdateView, DeleteView
 
 urlpatterns = [
-    path('home/', ProductList.as_view(), name = 'home')
+    path('home/', ProductList.as_view(), name='home'),
+    path('product/<int:pk>/', ProductView.as_view(), name='detail'),
+    path('product/add/', AddProductView.as_view(), name='add'),
+    path('product/update/<int:pk>/', UpdateView.as_view(), name='update'),
+    path('product/delete/<int:pk>/', DeleteView.as_view(), name='delete'),
 ]
--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product, Review
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from .forms import ProductForm
 
 
 # Create your views here.
@@ -12,7 +13,66 @@ class ProductList(ListView):
     context_object_name = "products"
     paginated_by = 5
 
+class ProductView(TemplateView):
+    template_name = "detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = Product.objects.get(pk=context['pk'])
+        print(context)
+        return context
+
+class AddProductView(TemplateView):
+    template_name = "add.html"
+    form = ProductForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_form'] = self.form()
+        return context
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST, request.FILES)
+        print(form)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return redirect('home')
+        return render(request, 'add.html', {'my_form': form})
 
 
+class UpdateView(TemplateView):
+    template_name = "add.html"
+    form = ProductForm
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        obj = Product.objects.get(pk=context['pk'])
+        context['my_form'] = self.form(
+            initial={'name': obj.name, 'category': obj.category, 'description': obj.description,
+                     'image': obj.image})
+        return context
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST,  request.FILES)
+        if form.is_valid():
+            obj = Product.objects.get(pk=kwargs['pk'])
+            obj.name = form.cleaned_data['name']
+            obj.category = form.cleaned_data['category']
+            obj.description = form.cleaned_data['description']
+            obj.image = form.cleaned_data['image']
+            obj.save()
+            return redirect('home')
+        return render(request, 'add.html', {'my_form': form})
 
 
+class DeleteView(TemplateView):
+    template_name = 'delete.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        obj = Product.objects.get(pk=context['pk'])
+        context['product'] = obj
+        return context
+    def post(self, request, *args, **kwargs):
+        obj = Product.objects.get(pk=kwargs['pk'])
+        if obj:
+            obj.delete()
+            return redirect('home')
+        return render (request,'delete.html', {'task':obj})
