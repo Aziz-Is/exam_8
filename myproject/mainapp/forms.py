71ab02e896567c81a2d0e982226a745de2d2b27d
Aziz_Is <Aziz10877@gmail.com>
2022-08-27 18:46:54 +0600

    auth added

--- myproject/mainapp/forms.py
+++ myproject/mainapp/forms.py
@@ -11,3 +11,30 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         exclude = []
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(label="Пароль", strip=False, required=True,
+                               widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", required=True, widget=forms.PasswordInput, strip=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+        if password and password_confirm and password_confirm != password:
+            raise forms.ValidationError('Пароли не совпадают')
+
+        first_name = cleaned_data.get("first_name")
+        print(first_name)
+        if first_name == '':
+            raise forms.ValidationError('Заполните имя')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -3,6 +3,10 @@ from .models import Product, Review
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from .forms import ProductForm, ReviewForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
+
 
 
 # Create your views here.
@@ -139,3 +143,30 @@ class DeleteReview(TemplateView):
             obj.delete()
             return redirect('home')
         return render(request, 'delete_review.html', {'task': obj})
+
+def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            context['has error'] = True
+    return render (request, 'login.html', context=context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def register_user(request, *args, **kwargs):
+    if request.method == 'POST':
+        my_form = RegisterForm(request.POST)
+        if my_form.is_valid():
+            my_form.save()
+            return redirect('login')
+    else:
+        my_form = RegisterForm()
+    return render(request, 'register.html', {'form': my_form})
